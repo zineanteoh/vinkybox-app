47e9023c017a69effb3cd79711bc34c30f851cfd
Zi Nean Teoh <zinean00@gmail.com>
2021-12-18 22:51:44 -0600

    uncommented methods and added deliveryRequest collection reference

--- lib/api/firestore_api.dart
+++ lib/api/firestore_api.dart
@@ -1,21 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:vinkybox/app/app.logger.dart';
 import 'package:vinkybox/constants/app_keys.dart';
 import 'package:vinkybox/exceptions/firestore_api_exception.dart';
+import 'package:vinkybox/models/app_user.dart';
 
+// Performs APIs relating to Cloud Firestore NoSQL database
+// ... add user info to collection
+// ... send & retrieve delivery requests
 class FirestoreApi {
   final log = getLogger('FirestoreApi');
 
   final CollectionReference usersCollection =
       FirebaseFirestore.instance.collection(UsersFirestoreKey);
 
-  Future<void> createUser(User? user) async {
+  final CollectionReference deliveryRequestsCollection =
+      FirebaseFirestore.instance
+          .collection(DeliveryRequestsFirestoreKey);
+
+  /// createUser adds an AppUser document to collection
+  Future<void> createUser(AppUser user) async {
     log.i('user:$user');
 
     try {
-      final userDocument = usersCollection.doc(user!.uid);
-      // await userDocument.set(user.toJson());
+      final userDocument = usersCollection.doc(user.id);
+      await userDocument.set(user.toJson());
       log.v('UserCreated at ${userDocument.path}');
     } catch (error) {
       throw FirestoreApiException(
@@ -25,24 +33,27 @@ class FirestoreApi {
     }
   }
 
-  // Future<User?> getUser({required String userId}) async {
-  //   log.i('userId:$userId');
+  /// getUser returns an AppUser from firestore
+  Future<AppUser?> getUser({required String userId}) async {
+    log.i('userId:$userId');
 
-  //   if (userId.isNotEmpty) {
-  //     final userDoc = await usersCollection.doc(userId).get();
-  //     if (!userDoc.exists) {
-  //       log.v('We have no user with id $userId in our database');
-  //       return null;
-  //     }
+    if (userId.isNotEmpty) {
+      final userDoc = await usersCollection.doc(userId).get();
+      if (!userDoc.exists) {
+        log.v('We have no user with id $userId in our database');
+        return null;
+      }
 
-  //     final userData = userDoc.data() as Map<String, dynamic>;
-  //     log.v('User found. Data: $userData');
+      final userData = userDoc.data() as Map<String, dynamic>;
+      log.v('User found. Data: $userData');
 
-  //     return User.fromJson(userData);
-  //   } else {
-  //     throw FirestoreApiException(
-  //         message:
-  //             'Your userId passed in is empty. Please pass in a valid user if from your Firebase user.');
-  //   }
-  // }
+      return AppUser.fromData(userData);
+    } else {
+      throw FirestoreApiException(
+          message:
+              'Your userId passed in is empty. Please pass in a valid user if from your Firebase user.');
+    }
+  }
+
+  /// addNewDeliveryRequest
 }
