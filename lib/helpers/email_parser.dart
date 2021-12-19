da23c9342b18a022605bf19b92f0814559769bbb
Zi Nean Teoh <zinean00@gmail.com>
2021-12-18 22:49:32 -0600

    added helper methods for parsing email to get first and last name

---
+++ lib/helpers/email_parser.dart
@@ -0,0 +1,21 @@
+import 'package:vinkybox/app/app.logger.dart';
+
+// Assumes email is in the form of
+// e.g. firstname.middle.lastname@domain.com
+String getFullNameFromEmail(String email) {
+  final log = getLogger('EmailExtensions');
+  try {
+    var names = email.split('@')[0].split('.');
+    // e.g. name: 'firstname.middle.lastname' or 'firstname.lastname'
+    String firstName = names[0];
+    String lastName = names[names.length - 1];
+    return capitalize(firstName) + " " + capitalize(lastName);
+  } catch (e) {
+    log.e(e);
+    return "";
+  }
+}
+
+String capitalize(String word) {
+  return "${word[0].toUpperCase()}${word.substring(1).toLowerCase()}";
+}
