7533f15144683abae8bd79885dcfa4bad49bd12c
Zi Nean Teoh <zinean00@gmail.com>
2021-12-21 13:40:37 +0800

    top profile bar now shows circula profile with ripple click effect

--- lib/ui/widgets/dumb_widgets/top_profile_bar.dart
+++ lib/ui/widgets/dumb_widgets/top_profile_bar.dart
@@ -9,21 +9,31 @@ class TopProfileBar extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.only(top: 10.0, right: 30.0),
         alignment: Alignment.centerRight,
-        height: 60,
-        // decoration: BoxDecoration(
-        //   color: Colors.black,
-        //   borderRadius: BorderRadius.circular(50 / 2),
-        // ),
-        child: const Image(
-          image: AssetImage('assets/images/temp_profile.png'),
+        child: Material(
+          shape: const CircleBorder(),
+          clipBehavior: Clip.hardEdge,
+          color: Colors.transparent,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30.0),
+            child: Ink.image(
+                width: 60,
+                height: 60,
+                image: const AssetImage(
+                    'assets/images/temp_profile.png'),
+                child: InkWell(
+                  onTap: () {
+                    print('clicked');
+                  },
+                )),
+          ),
+          // child: CachedNetworkImage(
+          //   imageUrl:
+          //       "https://secure.gravatar.com/avatar/ef4a9338dca42372f15427cdb4595ef7",
+          //   placeholder: (context, url) =>
+          //       new CircularProgressIndicator(),
+          //   errorWidget: (context, url, error) => new Icon(Icons.error),
+          // ),
         ),
-        // child: CachedNetworkImage(
-        //   imageUrl:
-        //       "https://secure.gravatar.com/avatar/ef4a9338dca42372f15427cdb4595ef7",
-        //   placeholder: (context, url) =>
-        //       new CircularProgressIndicator(),
-        //   errorWidget: (context, url, error) => new Icon(Icons.error),
-        // ),
       ),
     );
   }
