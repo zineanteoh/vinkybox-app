12cf15361fba1ce3450f3252fd19988f36cff7ae
Zi Nean Teoh <zinean00@gmail.com>
2021-12-22 09:45:55 +0800

    changed request delivery button from stateful to stateless w/ viewmodelbuilder

--- lib/ui/widgets/dumb_widgets/delivery_overview.dart
+++ lib/ui/widgets/dumb_widgets/delivery_overview.dart
@@ -1,6 +1,4 @@
-import 'package:carbon_icons/carbon_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:vinkybox/ui/shared/app_colors.dart';
 import 'package:vinkybox/ui/shared/ui_helpers.dart';
 import 'package:vinkybox/ui/widgets/smart_widgets/request_delivery_button.dart/request_delivery_button.dart';
 
@@ -16,12 +14,7 @@ class DeliveryOverview extends StatelessWidget {
             UIHelper.verticalSpaceLarge(),
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: RequestDeliveryButton(
-                icon: CarbonIcons.delivery_parcel,
-                iconBgColor: skyblueHex,
-                title: "Request Delivery",
-                description: "Too busy? Let others deliver for you!",
-              ),
+              child: const RequestDeliveryButton(),
             ),
             // List of deliveries
             // Expanded(
--- lib/ui/widgets/smart_widgets/request_delivery_button.dart/request_delivery_button_model.dart
+++ lib/ui/widgets/smart_widgets/request_delivery_button.dart/request_delivery_button_model.dart
@@ -1,5 +1,19 @@
+import 'package:carbon_icons/carbon_icons.dart';
+import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:vinkybox/ui/shared/app_colors.dart';
 
 class RequestDeliveryButtonModel extends BaseViewModel {
   bool _pressed = false;
+  bool get pressed => _pressed;
+
+  void updatePressedStatus(tapState) {
+    _pressed = tapState;
+    notifyListeners();
+  }
+
+  final IconData icon = CarbonIcons.delivery_parcel;
+  final Color iconBgColor = skyblueHex;
+  final String title = "Request Delivery";
+  final String description = "Too busy? Let others deliver for you!";
 }
