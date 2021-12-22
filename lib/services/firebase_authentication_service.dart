6d3965d61153553233ec2fd61d4d491767ee93c0
Zi Nean Teoh <zinean00@gmail.com>
2021-12-23 07:55:40 +0800

    added google_sign_in version 4.5.9 for now (5.0.0 above doesn't work for my m1)

--- lib/services/firebase_authentication_service.dart
+++ lib/services/firebase_authentication_service.dart
@@ -1,15 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:vinkybox/api/firestore_api.dart';
 import 'package:vinkybox/app/app.locator.dart';
 import 'package:vinkybox/app/app.logger.dart';
+import 'package:vinkybox/models/application_models.dart';
 // import 'package:vinkybox/models/application_models.dart';
 
 // Performs Google_Sign_In authentication with Firebase here
 class FirebaseAuthenticationService {
   final log = getLogger('FirebaseAuthenticationService');
 
-  // final _googleSignIn = GoogleSignIn();
+  final _googleSignIn = GoogleSignIn();
 
   final _firestoreApi = locator<FirestoreApi>();
 
@@ -17,39 +18,41 @@ class FirebaseAuthenticationService {
 
   Future<FirebaseAuthenticationResult> signInWithGoogle() async {
     try {
-      // // Trigger the authentication flow
-      // final GoogleSignInAccount? googleUser =
-      //     await _googleSignIn.signIn();
-      // if (googleUser == null) {
-      //   log.i('Process is canceled by the user');
-      //   return FirebaseAuthenticationResult.error(
-      //       errorMessage:
-      //           'Google Sign In has been cancelled by the user');
-      // }
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser =
+          await _googleSignIn.signIn();
 
-      // // Obtain the auth details from the request
-      // final GoogleSignInAuthentication googleSignInAuth =
-      //     await googleUser.authentication;
+      if (googleUser == null) {
+        log.i('Process is canceled by the user');
+        return FirebaseAuthenticationResult.error(
+            errorMessage:
+                'Google Sign In has been cancelled by the user');
+      }
 
-      // // Create a new credential
-      // final AuthCredential credential = GoogleAuthProvider.credential(
-      //   accessToken: googleSignInAuth.accessToken,
-      //   idToken: googleSignInAuth.idToken,
-      // );
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication googleSignInAuth =
+          await googleUser.authentication;
 
-      // // Once signed in, store the UserCredential
-      // final result =
-      //     await firebaseAuth.signInWithCredential(credential);
+      // Create a new credential
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuth.accessToken,
+        idToken: googleSignInAuth.idToken,
+      );
 
-      // // Add user to Cloud Firestore
-      // _firestoreApi.createUser(Models.User(
-      //     id: result.user!.uid, email: result.user!.email));
+      // Once signed in, store the UserCredential
+      final result =
+          await firebaseAuth.signInWithCredential(credential);
 
-      // // Return result
-      // return FirebaseAuthenticationResult(user: result.user);
-      log.i("Testing only");
-      return FirebaseAuthenticationResult.error(
-          errorMessage: "testing only");
+      // Add user to Cloud Firestore
+      _firestoreApi.createUser(
+        user: AppUser(
+          id: result.user!.uid,
+          email: result.user!.email,
+        ),
+      );
+
+      // Return result
+      return FirebaseAuthenticationResult(user: result.user);
     } catch (e) {
       log.e(e);
       return FirebaseAuthenticationResult.error(
--- lib/ui/views/login/login_viewmodel.dart
+++ lib/ui/views/login/login_viewmodel.dart
@@ -1,19 +1,39 @@
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+// import 'package:stacked_services/stacked_services.dart';
 import 'package:vinkybox/app/app.locator.dart';
+import 'package:vinkybox/app/app.logger.dart';
 import 'package:vinkybox/app/app.router.dart';
+// import 'package:vinkybox/app/app.router.dart';
 import 'package:vinkybox/services/firebase_authentication_service.dart';
-// import 'package:vinkybox/services/firebase_authentication_service.dart';
 
 class LoginViewModel extends BaseViewModel {
-  // final _firebaseAuthenticationService = locator<FirebaseAuthenticationService>();
+  final _navigationService = locator<NavigationService>();
 
-  void loginWithGoogle() {
+  final _firebaseAuthenticationService =
+      locator<FirebaseAuthenticationService>();
+
+  final log = getLogger('LoginViewModel');
+
+  Future loginWithGoogle() async {
     // Uncomment the below
-    // _firebaseAuthenticationService.signInWithGoogle();
+    final result =
+        await _firebaseAuthenticationService.signInWithGoogle();
+    _handleAuthenticationResponse(result);
 
     // Temporary
-    final _navigationServce = locator<NavigationService>();
-    _navigationServce.navigateTo(Routes.onboardingView);
+    // final _navigationServce = locator<NavigationService>();
+    // _navigationServce.navigateTo(Routes.onboardingView);
+  }
+
+  void _handleAuthenticationResponse(
+      FirebaseAuthenticationResult result) {
+    if (!result.hasError) {
+      // if first time, go to onboarding view -> dorm_selection
+      _navigationService.replaceWith(Routes.onboardingView);
+      // TODO: else go to home page
+    } else {
+      log.e('Error signing in with Google.');
+    }
   }
 }
