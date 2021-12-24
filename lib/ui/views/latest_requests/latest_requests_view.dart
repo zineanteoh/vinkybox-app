cbef755a479d3a14a901b1c480f87d249e0e7615
Zi Nean Teoh <zinean00@gmail.com>
2021-12-24 14:51:40 +0800

    added basic ui design to request item

--- lib/ui/views/latest_requests/latest_requests_view.dart
+++ lib/ui/views/latest_requests/latest_requests_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:styled_widget/styled_widget.dart';
+import 'package:vinkybox/ui/shared/app_colors.dart';
+import 'package:vinkybox/ui/shared/text_styles.dart';
 import 'package:vinkybox/ui/shared/ui_helpers.dart';
 import 'package:vinkybox/ui/views/latest_requests/latest_requests_viewmodel.dart';
 import 'package:vinkybox/ui/widgets/dumb_widgets/header_bar.dart';
@@ -9,16 +12,66 @@ class LatestRequestsView extends StatelessWidget {
 
   Widget _buildRequestList() {
     return ListView.builder(
-      itemCount: 20,
+      itemCount: 100,
       itemBuilder: (context, index) => _buildRequestItem(index),
       shrinkWrap: false,
     );
   }
 
   Widget _buildRequestItem(index) {
-    return ListTile(
-      title: Text('hello $index'),
+    return requestItem(
+      child: <Widget>[
+        // Name
+        const Text(
+          'Zi Teoh',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ).padding(top: 12, left: 12),
+        // Time
+        const Text(
+          '2:50PM, Jan 1',
+          style: TextStyle(
+            fontSize: 14,
+            color: Colors.black38,
+          ),
+        ).padding(top: 4, left: 12),
+        // Pick up & drop off
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Text('Station B Counter',
+                style: TextStyle(
+                    color: skyblueColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold)),
+            Text('West',
+                style: TextStyle(
+                    color: skyblueColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold)),
+          ],
+        ).padding(all: 12),
+        // Status
+        const Center(
+            child: Text(
+          'Status: new',
+          style: TextStyle(
+            fontSize: 18,
+            // color: limeGreenColor,
+          ),
+        )).padding(horizontal: 20, vertical: 12),
+      ].toColumn(
+        crossAxisAlignment: CrossAxisAlignment.start,
+      ),
     );
+  }
+
+  Widget requestItem({required Widget child}) {
+    return Styled.widget(child: child)
+        .backgroundColor(Colors.white)
+        .padding(vertical: 10)
+        .borderRadius(all: 10);
   }
 
   @override
