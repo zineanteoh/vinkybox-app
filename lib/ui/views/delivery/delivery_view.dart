78fc4cf38d45ff37f5e85609e8e4c6556f5a5081
Zi Nean Teoh <zinean00@gmail.com>
2021-12-26 13:25:00 +0800

    added request preview to deliery view

--- lib/ui/views/delivery/delivery_view.dart
+++ lib/ui/views/delivery/delivery_view.dart
@@ -3,38 +3,44 @@ import 'package:stacked/stacked.dart';
 import 'package:vinkybox/ui/shared/ui_helpers.dart';
 import 'package:vinkybox/ui/views/delivery/delivery_viewmodel.dart';
 import 'package:vinkybox/ui/widgets/dumb_widgets/latest_requests_header.dart';
+import 'package:vinkybox/ui/widgets/smart_widgets/delivery_request_item/delivery_request_item.dart';
 import 'package:vinkybox/ui/widgets/smart_widgets/my_packages_button/my_packages_button.dart';
 import 'package:vinkybox/ui/widgets/smart_widgets/request_delivery_button/request_delivery_button.dart';
 import 'package:vinkybox/ui/widgets/smart_widgets/top_profile_bar/top_profile_bar.dart';
 import 'package:vinkybox/ui/widgets/smart_widgets/welcome_message/welcome_message.dart';
 
 class DeliveryView extends StatelessWidget {
-  DeliveryView({Key? key}) : super(key: key);
+  const DeliveryView({Key? key}) : super(key: key);
 
-  // Widget latestRequestsPreview = Expanded(
-  //   child: ,
-  // );
+  Widget latestRequestsPreview(DeliveryViewModel model) {
+    return Container(
+      child: model.isBusy
+          ? const CircularProgressIndicator()
+          : DeliveryRequestItem(
+              deliveryRequest: model.deliveryRequestList[0]),
+    );
+  }
 
-  Widget latestRequest = Expanded(
-    child: Scrollbar(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          UIHelper.verticalSpaceMedium(),
-          const RequestDeliveryButton(),
-          UIHelper.verticalSpaceMedium(),
-          const MyPackagesButton(),
-          UIHelper.verticalSpaceMedium(),
-          UIHelper.verticalSpaceMedium(),
-          const LatestRequestsHeader(),
-        ],
-      ),
-    ),
-  );
+  Widget latestRequest() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        UIHelper.verticalSpaceMedium(),
+        const RequestDeliveryButton(),
+        UIHelper.verticalSpaceMedium(),
+        const MyPackagesButton(),
+        UIHelper.verticalSpaceMedium(),
+        UIHelper.verticalSpaceMedium(),
+        const LatestRequestsHeader(),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<DeliveryViewModel>.reactive(
+      onModelReady: (model) => model.loadLatestRequests(),
       builder: (context, model, child) => Scaffold(
         body: Column(
           children: [
@@ -46,9 +52,8 @@ class DeliveryView extends StatelessWidget {
                 WelcomeMessage(),
               ],
             ),
-            latestRequest,
-            // top 2 requests
-            // latestRequestsPreview,
+            latestRequest(),
+            latestRequestsPreview(model),
           ],
         ),
       ),
--- lib/ui/views/delivery/delivery_viewmodel.dart
+++ lib/ui/views/delivery/delivery_viewmodel.dart
@@ -1,5 +1,19 @@
 import 'package:stacked/stacked.dart';
+import 'package:vinkybox/app/app.locator.dart';
+import 'package:vinkybox/services/delivery_service.dart';
 
 class DeliveryViewModel extends BaseViewModel {
   // share
+  final _deliveryService = locator<DeliveryService>();
+
+  List<dynamic> get deliveryRequestList =>
+      _deliveryService.deliveryRequestList;
+
+  Future loadLatestRequests() async {
+    setBusy(true);
+    await Future.delayed(const Duration(milliseconds: 1000));
+    _deliveryService.fetchDeliveryRequestList();
+    notifyListeners();
+    setBusy(false);
+  }
 }
