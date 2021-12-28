38ddfe6ba9c742e0a340a445ca112802a62e0e5b
Zi Nean Teoh <zinean00@gmail.com>
2021-12-28 08:46:57 +0800

    configured android and ios for google_maps_flutter package

--- lib/ui/views/latest_requests/latest_requests_view.dart
+++ lib/ui/views/latest_requests/latest_requests_view.dart
@@ -40,7 +40,7 @@ class LatestRequestsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<LatestRequestsViewModel>.reactive(
-      onModelReady: (model) => model.loadLatestRequests(),
+      // onModelReady: (model) => model.loadLatestRequests(),
       builder: (context, model, child) => Scaffold(
         body: SafeArea(
           child: Container(
