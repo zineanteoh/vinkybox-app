10a5c61c5f59cf6bde8ab77d98242fab2b662e03
Zi Nean Teoh <zinean00@gmail.com>
2022-01-02 20:41:07 +0800

    added scroll to refresh to my packages view

--- lib/ui/views/delivery/my_packages/my_packages_view.dart
+++ lib/ui/views/delivery/my_packages/my_packages_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:stacked/stacked.dart';
+import 'package:styled_widget/styled_widget.dart';
 import 'package:vinkybox/ui/views/delivery/my_packages/my_packages_viewmodel.dart';
 import 'package:vinkybox/ui/widgets/dumb_widgets/header_bar.dart';
 
@@ -11,24 +13,32 @@ class MyPackagesView extends StatelessWidget {
     return ViewModelBuilder<MyPackagesViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20.0, top: 50.0),
-            child: Column(
-              children: [
-                HeaderBar(
-                  model: model,
-                  headerText: "My Packages",
-                ),
-                // UIHelper.verticalSpaceMedium(),
-                // requestPackageSizeSection(model),
-                // requestPickUpLocationSection(model),
-                // requestDropOffLocationSection(model),
-                // requestTimeSection(model),
-                // UIHelper.verticalSpaceMedium(),
-                // requestSubmit(model),
-              ],
+          child: <Widget>[
+            HeaderBar(
+              model: model,
+              headerText: "My Packages",
             ),
-          ),
+            Expanded(
+              child: SmartRefresher(
+                header: const ClassicHeader(
+                  completeText: 'My packages is up to date!',
+                  idleText: 'Pull to Refresh',
+                  refreshingText: 'Fetching My Packages...',
+                ),
+                enablePullDown: true,
+                controller: model.refreshController,
+                onRefresh: model.onRefresh,
+                child: ListView.builder(
+                  itemCount: 0,
+                  itemBuilder: (context, index) => Column(
+                    children: [
+                      // my packages children
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ].toColumn().padding(left: 20.0, top: 50.0),
         ),
       ),
       viewModelBuilder: () => MyPackagesViewModel(),
--- lib/ui/views/delivery/my_packages/my_packages_viewmodel.dart
+++ lib/ui/views/delivery/my_packages/my_packages_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:vinkybox/app/app.locator.dart';
@@ -12,5 +13,24 @@ class MyPackagesViewModel extends BaseViewModel {
 
   void navigateBack() {
     _navigationService.back();
+  }
+
+  final RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
+  RefreshController get refreshController => _refreshController;
+
+  Future loadLatestRequests() async {
+    setBusy(true);
+    await Future.delayed(const Duration(milliseconds: 1000));
+    setBusy(false);
+  }
+
+  Future onRefresh() async {
+    await Future.delayed(const Duration(milliseconds: 1000));
+    // _deliveryService.fetchDeliveryRequestList();
+    // notifyListeners();
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
   }
 }
